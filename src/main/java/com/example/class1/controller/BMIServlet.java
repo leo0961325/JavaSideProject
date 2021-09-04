9c0e869b9471bfc39ba1a3ede8f82213ab5dc878
leo0961325 <47008876+leo0961325@users.noreply.github.com>
2021-09-04 21:53:08 +0800

    編碼模式enctype　form-data

---
+++ src/main/java/com/example/class1/controller/BMIServlet.java
@@ -0,0 +1,33 @@
+package com.example.class1.controller;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
+@WebServlet("/bmi")
+public class BMIServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        req.setCharacterEncoding("UTF-8");
+        resp.setCharacterEncoding("UTF-8");
+        resp.setContentType("text/html;charset=utf-8");
+
+        PrintWriter out = resp.getWriter();
+
+        // 顯示內容：HTTP 文件 (Part 4)
+        InputStreamReader isr = new InputStreamReader(req.getInputStream());
+        char[] buffer = new char[1];
+        while (isr.read(buffer) != -1) {
+            out.print(new String(buffer));
+        }
+    }
+}
